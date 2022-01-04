80542e52f6e46bb81ba77721966775056212a5f7
pixelmund <pixelmund@gmail.com>
2022-01-04 17:33:09 +0100

    Fix typo

--- src/lib/plugin/pipeline/pipes/routes.ts
+++ src/lib/plugin/pipeline/pipes/routes.ts
@@ -11,7 +11,7 @@ const RoutesPipe: Pipe = async function (args) {
 		fileName = fileName.replace('__layout.svelte', '$__layout.svelte');
 	}
 	if (fileName.includes('__layout.reset.svelte')) {
-		fileName = fileName.replace('__layout.reset-svelte', '$__layout_reset.svelte');
+		fileName = fileName.replace('__layout.reset.svelte', '$__layout_reset.svelte');
 	}
 
 	fileName = fileName.split('/');
