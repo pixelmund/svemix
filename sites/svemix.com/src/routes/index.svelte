b0ba232a9a4e616a463d4ca9cea38d5490d5492b
Julian Sigmund <58262143+pixelmund@users.noreply.github.com>
2022-02-20 23:24:30 +0100

    Update index.svelte

--- sites/svemix.com/src/routes/index.svelte
+++ sites/svemix.com/src/routes/index.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
 <svelte:head>
 	<title>The Full-Stack addition to SvelteKit • Svemix</title>
 	<meta name="twitter:title" content="The Full-Stack addition to SvelteKit • Svemix" />
